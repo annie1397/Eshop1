47da6348ce8e697623a49f3247dc91ab21d2cbd8
Maria Giannakou <mariagiannakou99@yahoo.com>
2019-05-09 17:44:29 +0300

    css

---
+++ Eshop.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+?>
+
+<html>
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+	<link rel ="stylesheet" href ="style.css"/>
+
+</head>
+<body>
+
+
+<div class="poikilies">
+ <h1> Ποικιλίες Καφέ </h1>
+ 
+
+
+
+
+</body>
+</html>
