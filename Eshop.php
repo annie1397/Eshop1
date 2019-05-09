4fd76998b3273751310400885da73290b6358a19
vatsoba <vatsoba@teiemt.gr>
2019-05-09 19:32:42 +0300

    kafes

--- Eshop.php
+++ Eshop.php
@@ -12,6 +12,100 @@
     <meta charset="UTF-8">
 	<link rel ="stylesheet" href ="style.css"/>
 
+	<div class="picture">
+  <a target="_blank" href="AITHIOPIA(2).jpg">
+    <img src="AITHIOPIA(2).jpg" alt="AITHIOPIA(2)" >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+<div class="picture">
+  <a target="_blank" href="AITHIOPIA.jpg">
+    <img src="AITHIOPIA.jpg" alt="AITHIOPIA" >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+<div class="picture">
+  <a target="_blank" href="BRAVO.jpg">
+  <a target="_blank" href="BRAVO.jpg">
+    <img src="BRAVO.jpg" alt="BRAVO" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+<div class="picture">
+  <a target="_blank" href="BRISTOT.jpg">
+    <img src="BRISTOT.jpg" alt="BRISTOT" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+ 
+	<div class="picture">
+  <a target="_blank" href="BUONDI.jpg">
+    <img src="BUONDI.jpg" alt="BUONDI" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+<div class="picture">
+  <a target="_blank" href="CAGLIARI(2).jpg">
+    <img src="CAGLIARI(2).jpg" alt="CAGLIARI(2)" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+	<div class="picture">
+  <a target="_blank" href="CAGLIARI.jpg">
+    <img src="CAGLIARI.jpg" alt="CAGLIARI" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+		<div class="picture">
+  <a target="_blank" href="CAGLIARI(3).jpg">
+    <img src="CAGLIARI(3).jpg" alt="CAGLIARI(3)" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+	<div class="picture">
+  <a target="_blank" href="COSMAI(2).jpg">
+    <img src="COSMAI(2).jpg" alt="COSMAI(2)" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+	
+	<div class="picture">
+  <a target="_blank" href="COSMAI.jpg">
+    <img src="COSMAI.jpg" alt="COSMAI" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+<div class="picture">
+  <a target="_blank" href="COSMAI(3).jpg">
+    <img src="COSMAI(3).jpg" alt="COSMAI(3)" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+<div class="picture">
+  <a target="_blank" href="COVIM(2).jpg">
+    <img src="COVIM(2).jpg" alt="COVIM(2)" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+
+<div class="picture">
+  <a target="_blank" href="COVIM(3).jpg">
+    <img src="COVIM(3).jpg" alt="COVIM(3)" height =100% >
+  </a>
+  <div class="desc">Εκλεκτός καφές</div>
+</div>
+	
+	
 </head>
 <body>
 
