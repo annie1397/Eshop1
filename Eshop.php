4948be152d6962b3f6fc9a1ab9f82e2b3537679e
Maria Giannakou <mariagiannakou99@yahoo.com>
2019-05-09 19:29:56 +0300

    kati

--- Eshop.php
+++ Eshop.php
@@ -12,16 +12,149 @@
     <meta charset="UTF-8">
 	<link rel ="stylesheet" href ="style.css"/>
 
+
+
 </head>
 <body>
 
 
-<div class="poikilies">
+ <div class="poikilies">
  <h1> Ποικιλίες Καφέ </h1>
- 
+</div>
+<nav class="product-filter">
+		<h1>Coffee</h1>
+		
+		<div class="sort">
+			<div class="collection-sort">
+				<label>Filter by:</label>
+				<select>
+		      <option value="/">Καφέδες</option>
+		      <option value="/">Μηχανές Καφέ</option>
+		      <option value="/">Κάψουλες Μηχανής</option>
+		      
+		   	</select>
+			</div>
+			
+			<div class="collection-sort">
+				<label>Sort by:</label>
+				<select>
+		      <option value="/">Featured</option>
+		      <option value="/">Best Selling</option>
+		      <option value="/">Alphabetically, A-Z</option>
+		      <option value="/">Alphabetically, Z-A</option>
+		      <option value="/">Price, low to high</option>
+		      <option value="/">Price, high to low</option>
+		      <option value="/">Date, new to old</option>
+		      <option value="/">Date, old to new</option>
+		    </select>
+			</div>
+		</div>
+	</nav>
+	
+	<section class="products">
+		<div class="product-card">
+			<div class="product-image">
+				<img src="Julius Meinl.jpg>
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+		<div class="product-card">
+			<div class="product-image">
+				<img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369">
+			</div>
+			<div class="product-info">
+				<h5>Winter Jacket</h5>
+				<h6>$99.99</h6>
+			</div>
+		</div>
+		
+	</section>
+  	
 
-
-
-
+		
 </body>
 </html>
