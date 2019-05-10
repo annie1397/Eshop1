37df10da6e802544b10add74d6b0f2988bcf42f7
vatsoba <vatsoba@teiemt.gr>
2019-05-10 15:22:38 +0300

    times

--- Eshop.php
+++ Eshop.php
@@ -16,14 +16,14 @@
   <a target="_blank" href="AITHIOPIA(2).jpg">
     <img src="AITHIOPIA(2).jpg" alt="AITHIOPIA(2)" >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">23,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="AITHIOPIA.jpg">
     <img src="AITHIOPIA.jpg" alt="AITHIOPIA" >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">20,00€</div>
 </div>
 
 <div class="picture">
@@ -31,14 +31,14 @@
   <a target="_blank" href="BRAVO.jpg">
     <img src="BRAVO.jpg" alt="BRAVO" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">25,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="BRISTOT.jpg">
     <img src="BRISTOT.jpg" alt="BRISTOT" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">15,00€</div>
 </div>
 
  
@@ -46,350 +46,350 @@
   <a target="_blank" href="BUONDI.jpg">
     <img src="BUONDI.jpg" alt="BUONDI" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">15,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="CAGLIARI(2).jpg">
     <img src="CAGLIARI(2).jpg" alt="CAGLIARI(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">18,00€</div>
 </div>
 
 	<div class="picture">
   <a target="_blank" href="CAGLIARI.jpg">
     <img src="CAGLIARI.jpg" alt="CAGLIARI" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">30,00€</div>
 </div>
 
 		<div class="picture">
   <a target="_blank" href="CAGLIARI(3).jpg">
     <img src="CAGLIARI(3).jpg" alt="CAGLIARI(3)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">17,00€</div>
 </div>
 
 	<div class="picture">
   <a target="_blank" href="COSMAI(2).jpg">
     <img src="COSMAI(2).jpg" alt="COSMAI(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">32,00€</div>
 </div>
 	
 	<div class="picture">
   <a target="_blank" href="COSMAI.jpg">
     <img src="COSMAI.jpg" alt="COSMAI" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">26,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="COSMAI(3).jpg">
     <img src="COSMAI(3).jpg" alt="COSMAI(3)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">19,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="COVIM(2).jpg">
     <img src="COVIM(2).jpg" alt="COVIM(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">21,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="COVIM(3).jpg">
     <img src="COVIM(3).jpg" alt="COVIM(3)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">20,00€</div>
 </div>
 	
 	<div class="picture">
   <a target="_blank" href="COVIM.jpg">
     <img src="COVIM.jpg" alt="COVIM(3)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">23,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DA VINCI(2).jpg">
     <img src="DA VINCI(2).jpg" alt="DA VINCI(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">16,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DA VINCI.jpg">
     <img src="DA VINCI.jpg" alt="DA VINCI" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">30,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DANESI.jpg">
     <img src="DANESI.jpg" alt="DANESI" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DANESI(2).jpg">
     <img src="DANESI(2).jpg" alt="DANESI(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">23,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DE ROCCIS(2).jpg">
     <img src="DE ROCCIS(2).jpg" alt="DE ROCCIS(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">24,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DE ROCCIS(3).jpg">
     <img src="DE ROCCIS(3).jpg" alt="DE ROCCIS(3)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DE ROCCIS.jpg">
     <img src="DE ROCCIS.jpg" alt="DE ROCCIS" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">21,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="DEL FARO.jpg">
     <img src="DEL FARO.jpg" alt="DEL FARO" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">20,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="EDUSCHO.jpg">
     <img src="EDUSCHO.jpg" alt="EDUSCHO" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">19,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="EDUSCHO(2).jpg">
     <img src="EDUSCHO(2).jpg" alt="EDUSCHO(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">18,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="GARIBALDI.jpg">
     <img src="GARIBALDI.jpg" alt="GARIBALDI" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="ICS(2).jpg">
     <img src="ICS(2).jpg" alt="ICS(2)" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">25,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="ICS.jpg">
     <img src="ICS.jpg" alt="ICS" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">26,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Ipanema.jpg">
     <img src="Ipanema.jpg" alt="Ipanema" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Jacobs.jpg">
     <img src="Jacobs.jpg" alt="Jacobs" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">29,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Julius Meinl.jpg">
     <img src="Julius Meinl.jpg" alt="Julius Meinl" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">25,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="KANABISSIMO.jpg">
     <img src="KANABISSIMO.jpg" alt="KANABISSIMO" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">12,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Lamborghini.jpg">
     <img src="Lamborghini.jpg" alt="Lamborghini" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">50,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Lavazza Blue.jpg">
     <img src="Lavazza Blue.jpg" alt="Lavazza Blue" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">26,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Lollo.jpg">
     <img src="Lollo.jpg" alt="Lollo" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">27,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Lucaffe.jpg">
     <img src="Lucaffe.jpg" alt="Lucaffe" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">20,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Manaresi.jpg">
     <img src="Manaresi.jpg" alt="Manaresi" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Melitta.jpg">
     <img src="Melitta.jpg" alt="Melitta" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">23,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Molinari.jpg">
     <img src="Molinari.jpg" alt="Molinari" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">27,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Monbana.jpg">
     <img src="Monbana.jpg" alt="Monbana" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">16,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Mondocaffejpg">
     <img src="Mondocaffe.jpg" alt="Mondocaffe" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">12,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Motta.jpg">
     <img src="Motta.jpg" alt="Motta" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">14,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Natreen.jpg">
     <img src="Natreen.jpg" alt="Natreen" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">18,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Nescafe.jpg">
     <img src="Nescafe.jpg" alt="Nescafe" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="New York.jpg">
     <img src="New York.jpg" alt="New York" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">28,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Paluani.jpg">
     <img src="Paluani.jpg" alt="Paluani" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">20,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Passalacqua.jpg">
     <img src="Passalacqua.jpg" alt="Passalacqua" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">24,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Pelican Rouge.jpg">
     <img src="Pelican Rouge.jpg" alt="Pelican Rouge" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">25,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Portioli.jpg">
     <img src="Portioli.jpg" alt="Portioli" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">20,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Puro.jpg">
     <img src="Puro.jpg" alt="Puro" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,50€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Ristora.jpg">
     <img src="Ristora.jpg" alt="Ristora" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">29,80€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Sandemetrio.jpg">
     <img src="Sandemetrio.jpg" alt="Sandemetrio" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">20,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Simat.jpg">
     <img src="Simat.jpg" alt="Simat" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">27,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Utopia.jpg">
     <img src="Utopia.jpg" alt="Utopia" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">21,00€</div>
 </div>
 
 <div class="picture">
   <a target="_blank" href="Vandino.jpg">
     <img src="Vandino.jpg" alt="Vandino" height =100% >
   </a>
-  <div class="desc">Εκλεκτός καφές</div>
+  <div class="desc">22,60€</div>
 </div>
 
 		
